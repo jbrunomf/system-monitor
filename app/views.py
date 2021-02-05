9060c26982f1556e9191c25a958415005334ac5e
João Bruno M. Fernandes <jbrunomf@outlook.com>
2021-02-05 17:39:30 -0400

    Added process list view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+import psutil
 
 # Create your views here.
+
+
+def processes(request):
+    """
+    :param request:
+    :return:
+    """
+    ctx = {"processes": psutil.process_iter(['pid', 'name', 'username', 'status', 'memory_percent', 'cpu_percent', 'memory_info'])}
+    return render(request, 'app/list.html', ctx)
+
+
+def process(request, pid):
+    ctx = {"process": psutil.Process(pid)}
+    return render(request, 'app/detail.html', ctx)
