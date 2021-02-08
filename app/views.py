ba2504bd26dc5fb79b54ed22bc93351892324451
João Bruno <jbrunomf@outlook.com>
2021-02-07 22:02:46 -0400

    added dashboard

--- app/views.py
+++ app/views.py
@@ -5,7 +5,37 @@ import psutil
 
 
 def index(request):
-    return render(request, 'app/index.html')
+    ctx = {
+        'cpu': {
+            'load_average': psutil.getloadavg(),
+            'cores': psutil.cpu_count(logical=False),
+            'threads': psutil.cpu_count(),
+            'current_freq': psutil.cpu_freq(),
+        },
+        'memory': {
+            'total': psutil.virtual_memory().total//1048576,
+            'available': psutil.virtual_memory().free//1048576,
+            'used': psutil.virtual_memory().used//1048576,
+            'percent_used': psutil.virtual_memory().percent
+        },
+        'network': {
+            'status': psutil.net_if_addrs()['Loopback Pseudo-Interface 1'][0][1]
+        },
+        'disk': {
+            'mountpoint': psutil.disk_partitions()[0][0],
+            'fstype': psutil.disk_partitions()[0][2],
+
+        },
+        'swap': {
+            'total': psutil.swap_memory().total//1048576,
+            'used': psutil.swap_memory().used//1048576,
+            'free': psutil.swap_memory().free//1048576
+        },
+        'users': psutil.users()[0]
+
+
+    }
+    return render(request, 'app/index.html', ctx)
 
 
 def processes(request):
